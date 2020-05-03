b873c9bbc4c42540d063b358f666af0f618bb2b0
Yangyeeun <kap12@naver.com>
2020-05-03 20:38:17 +0900

    first commit

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InputTest : MonoBehaviour
+{
+    [SerializeField] private Text TextMoney;
+    [SerializeField] private InputField InputMoney;
+    // Start is called before the first frame update
+    private int curMoney;
+    public void Input()
+    {
+        curMoney += int.Parse(InputMoney.text);
+        TextMoney.text=curMoney.ToString();
+
+}
+
+public void Output()
+    {
+        curMoney -= int.Parse(InputMoney.text);
+        TextMoney.text = curMoney.ToString();
+
+    }
+}
---
+++ Assets/Scripts/SliderTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderTest : MonoBehaviour
+{
+    [SerializeField] public Slider Opprtunity;
+    public GameObject EndPanel;
+    [SerializeField] private Text TextName;
+    [SerializeField] private Image ImageName;
+
+    private float SliderTime = 1f;
+    private float currentSliderTime = 0f;
+
+    private float currentTime = 10f;
+    private float delayTime = 10f;
+
+    private bool IsClick;
+    private bool IsCoolTime = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSliderTime = SliderTime;
+        EndPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Slider
+        
+            if (Opprtunity.value < 0f)
+            {
+                EndPanel.SetActive(true);
+            }
+        
+        //Image
+        if (IsCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            ImageName.fillAmount = currentTime / delayTime;
+            if (Opprtunity.value > 0f)
+            {
+                if (currentTime <= 0)
+                {
+                    IsCoolTime = false;
+                    currentTime = delayTime;
+                    ImageName.fillAmount = currentTime;
+                    Opprtunity.value -= 2f;
+                }
+            }
+            else
+            {
+                ImageName.fillAmount = 0;
+            }
+        }
+    }
+    public void ClickButton()
+    {
+        Opprtunity.value -= 2f;
+        IsClick = true;
+    }
+
+    public void Change()
+    {
+        
+        IsCoolTime = true;
+    }
+}
